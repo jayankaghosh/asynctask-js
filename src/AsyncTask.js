93b5757e96450e198102f1732560985e573b42e2
jayankaghosh <jayankaghosh@gmail.com>
2016-08-11 09:53:42 +0530

    cross browser compatibility issues fixed

--- src/AsyncTask.js
+++ src/AsyncTask.js
@@ -100,9 +100,14 @@ function AsyncTask(pre,background,post){
 			if(method == null){
 				method = "GET";
 			}
-			var xhr = new XMLHttpRequest();
+			var xhr;
+			if (window.XMLHttpRequest) {
+			    xhr = new XMLHttpRequest();
+			    } else {
+			    // code for IE6, IE5
+			    xhr = new ActiveXObject("Microsoft.XMLHTTP");
+			}
 			xhr.open(method,url,true);
-			xhr.bef
 			xhr.onreadystatechange = function(){
 				if(xhr.readyState == 4 && xhr.status == 200){
 					context.errorHandle(true,xhr.responseText);
